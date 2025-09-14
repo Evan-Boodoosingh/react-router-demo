97ef00b1aa765d88ad35a92f18a60638227b9397
Evan Boodoosingh <evan9496@gmail.com>
2025-09-14 16:23:55 -0400

    feat: enhance AboutMe and AboutUs components with nested routes and styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,12 @@ import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
 import AboutMe from "../AboutMe/AboutMe";
+import Contact from "../AboutMe/Contact";
+import MyStory from "../AboutMe/MyStory";
+import Hobbies from "../AboutMe/Hobbies"; 
 import AboutUs from "../AboutUs/AboutUs";
+import SiteHistory from "../AboutUs/SiteHistory";
+import SiteMission from "../AboutUs/SiteMission";
 
 
 function App() {
@@ -16,8 +21,15 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
-        <Route path="/about-me" element={<AboutMe />} />
-        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="my-story" element={<MyStory />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="site-history" element={<SiteHistory />} />
+          <Route path="site-mission" element={<SiteMission />} />
+        </Route>
       </Routes>
     </div>
   );
