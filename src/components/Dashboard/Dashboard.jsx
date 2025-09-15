550ba17b4eee50e7ab86c27f36474188f603e0e3
Evan Boodoosingh <evan9496@gmail.com>
2025-09-14 20:59:35 -0400

    feat: replace Links with buttons for navigation in Dashboard component

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,16 +1,23 @@
 import{ Link }from"react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 
 import "./Dashboard.css";
 
 function Dashboard() {
+
+  const navigate = useNavigate();
+
   return (
     <div className="dashboard">
       <h1>Emoji Critic — All About Emojis</h1>
       <p>
         The #1 Destination for Emoji Reviews on the World Wide Web Since 2020!
       </p>
-      <Link to="/reviews" className="dashboard-link">
+      <button className="dashboard-link" onClick={() => navigate("/reviews")}>Click here to see my latest reviews!</button>
+      <button className="dashboard-link" onClick={() => navigate("/about-me")}>   Click here to learn more about me!</button>
+      <button className="dashboard-link" onClick={() => navigate("/about-us")}> Click here to learn more about us!</button>
+      {/* <Link to="/reviews" className="dashboard-link">
        Click here to see my latest reviews!
       </Link>
       <Link to="/about-me" className="dashboard-link">
@@ -18,7 +25,7 @@ function Dashboard() {
       </Link>
       <Link to="/about-us" className="dashboard-link">
        Click here to learn more about us!
-      </Link>
+      </Link> */}
     </div>
   );
 }
